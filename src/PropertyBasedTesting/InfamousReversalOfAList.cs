636829c810ed98e1f201ccd66bc97159b424073b
Arialdo Martini <arialdomartini@gmail.com>
2023-08-25 00:18:12 +0200

    Correct and wrong reversal of a list

---
+++ src/PropertyBasedTesting/InfamousReversalOfAList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace PropertyBasedTesting;
+
+public class InfamousReversalOfAList
+{
+    IEnumerable<string> Reverse(IEnumerable<string> xs) => xs.Reverse();
+    
+
+    [Property]
+    void invariant_of_reversal(ImmutableArray<string> xs) =>
+        Assert.Equal(xs, (Reverse(Reverse(xs))));
+
+    
+    IEnumerable<string> WrongReverse(IEnumerable<string> xs) => xs;
+
+    [Property]
+    void collateral_properties_can_be_deceiving(ImmutableArray<string> xs) =>
+        Assert.Equal(xs, (WrongReverse(WrongReverse(xs))));
+}
