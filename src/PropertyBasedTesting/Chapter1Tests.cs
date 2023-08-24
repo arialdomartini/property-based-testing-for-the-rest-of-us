3fe57b7ee14e396055cc694099e2fea2fe57d614
Arialdo Martini <arialdomartini@gmail.com>
2023-08-24 16:15:33 +0200

    Generating users

--- src/PropertyBasedTesting/Chapter1Tests.cs
+++ src/PropertyBasedTesting/Chapter1Tests.cs
@@ -1,14 +1,12 @@
 using FsCheck;
 using FsCheck.Xunit;
-using Microsoft.FSharp.Collections;
 using Xunit;
-
 using PropertyBasedTesting.Resources;
 using static PropertyBasedTesting.AssertExtensions;
 using static PropertyBasedTesting.Resources.Category;
 
 namespace PropertyBasedTesting;
- 
+
 record Product(Guid Id, string Name, Category Category, decimal Price);
 
 public class Chapter1Tests
@@ -21,10 +19,10 @@ public class Chapter1Tests
     {
         var product = new Product(
             Id: Guid.NewGuid(),
-            Name: "The Little Schemer", 
-            Category: Books, 
+            Name: "The Little Schemer",
+            Category: Books,
             Price: 16.50M);
-    
+
         _repository.Save(product);
 
         var found = _repository.LoadById(product.Id);
@@ -42,7 +40,7 @@ public class Chapter1Tests
 
         return found == product;
     }
-    
+
     string fizzbuzz(int i) =>
         i switch
         {
@@ -51,7 +49,7 @@ public class Chapter1Tests
             _ when i % 5 == 0 => "buzz",
             _ => i.ToString()
         };
-    
+
     [Theory]
     [InlineData(15)]
     [InlineData(30)]
@@ -61,12 +59,12 @@ public class Chapter1Tests
     {
         Assert.Equal("fizzbuzz", fizzbuzz(multipleOf15));
     }
-    
+
     // The same, as a property (in different styles)
     [Property]
     Property all_the_multiples_of_15_return_fizzbuzz()
     {
-        var multiplesOf15 = Arb.From( 
+        var multiplesOf15 = Arb.From(
             Arb.Generate<int>()
                 .Select(i => i * 15));
 
@@ -80,14 +78,14 @@ public class Chapter1Tests
             from n in Arb.Generate<int>()
             let nn = n * 15
             select nn);
-        
+
         return Prop.ForAll(
-            multiplesOf15, 
+            multiplesOf15,
             n => fizzbuzz(n) == "fizzbuzz");
     }
-    
+
     [Property]
-    bool all_the_multiples_of_15_return_fizzbuzz_as_a_predicate(int n) => 
+    bool all_the_multiples_of_15_return_fizzbuzz_as_a_predicate(int n) =>
         fizzbuzz(n * 15) == "fizzbuzz";
 
     [Fact]
@@ -99,35 +97,35 @@ public class Chapter1Tests
             select nn);
 
         var property = Prop.ForAll(
-            multiplesOf15, 
+            multiplesOf15,
             n => fizzbuzz(n) == "fizzbuzz");
-        
+
         Check.QuickThrowOnFailure(property);
     }
-    
-    
+
+
     int Sum(int a, int b) => a + b;
-    
+
     [Property]
-    bool sum_is_commutative(int a, int b) => 
+    bool sum_is_commutative(int a, int b) =>
         a + b == b + a;
 
     [Property]
-    bool adding_zero_does_not_change_the_result(int a) => 
+    bool adding_zero_does_not_change_the_result(int a) =>
         a + 0 == a;
 
-    [Fact]
     void generates_strings_from_generator_of_chars()
     {
         Gen<char> chars = Arb.Generate<char>();
         Gen<string> strings = Gen.ListOf(chars).Select(string.Concat);
-        
-        var arbitraryStrings = Gen.Sample(1,100, strings).ToList();
-
-        AssertAreAllDifferent(arbitraryStrings);
     }
 
-    [Fact]
+    void generates_tuples_of_strings_from_generator_of_strings()
+    {
+        Gen<string> strings = Arb.Generate<string>();
+        Gen<Tuple<string, string>> tuples = Gen.Two(strings);
+    }
+
     void generates_random_Products()
     {
         Gen<Product> products =
@@ -136,19 +134,33 @@ public class Chapter1Tests
             from price in Arb.Generate<decimal>()
             from category in Arb.Generate<Category>()
             select new Product(Id: id, Name: name, Price: price, Category: category);
-        
-        var arbitraryProducts = Gen.Sample(1,100, products).ToList();
+    }
 
-        AssertAreAllDifferent(arbitraryProducts.Select(p => p.Id));
+    void example_of_generators()
+    {
+        Gen<bool> equallyDistributedBooleans =
+            Arb.Generate<bool>();
+
+        Gen<bool> tenMoreTrueValuesThanFalseOnes =
+            Gen.Frequency(new Tuple<int, Gen<bool>>[]
+            {
+                new(10, Gen.Constant(true)),
+                new(1, Gen.Constant(false))
+            });
+        
+        var tuplesWithDifferentElements =
+            Gen.Two(Gen.Choose(1, 100))
+                .Where(t => t.Item1 != t.Item2);
+    }
+
+    record User(int Id, string FirstName, string LastName);
+    
+    void generating_users()
+    {
+        Gen<User> users =
+            from firstName in Gen.Elements("Don", "Henrik", null)
+            from secondName in Gen.Elements("Syme", "Feldt")
+            from id in Gen.Choose(0, 1000)
+            select new User(id, firstName, secondName);
     }
 }
-
-internal static class AssertExtensions
-{
-    internal static bool AreAllDifferent<T>(this IEnumerable<T> ids) => ids.Count() == ids.Distinct().Count();
-
-    internal static void AssertAreAllDifferent<T>(IEnumerable<T> xs)
-    {
-        Assert.True(xs.AreAllDifferent());
-    }
-} 
